783f3580397015ce17f872805a8cc534638f17a7
fatih255 <fatih969692@gmail.com>
2022-09-15 19:06:45 +0300

    added clean method for clean selected styles

--- src/index.ts
+++ src/index.ts
@@ -2,30 +2,52 @@ type Props = {
   itemsClass: string;
   containerClass: string;
   cloudStyle?: string;
-  crossEffect: ({ style, classList }: { style: CSSStyleDeclaration; classList: DOMTokenList }) => void;
+  crossEffect: ({
+    style,
+    classList
+  }: {
+    style: CSSStyleDeclaration;
+    classList: DOMTokenList;
+  }) => void;
   onComplete: (elements?: Element[]) => void;
 };
 
 let runnedSelectWithCloud = false;
-export default function SelectWithCloud({ itemsClass, containerClass, cloudStyle, crossEffect, onComplete }: Props) {
+const notesInitialAttributes = {
+  class: "",
+  style: ""
+};
+export default function SelectWithCloud({
+  itemsClass,
+  containerClass,
+  cloudStyle,
+  crossEffect,
+  onComplete
+}: Props) {
   if (!runnedSelectWithCloud) {
-    const container = document.querySelector(`.${containerClass}`) as HTMLElement;
-    container.style.userSelect = 'none';
-    container.style.position = 'relative';
+    const container = document.querySelector(
+      `.${containerClass}`
+    ) as HTMLElement;
+    container.style.userSelect = "none";
+    container.style.position = "relative";
     container.draggable = false;
-    const cloudSelection = document.createElement('div') as HTMLDivElement;
+    const cloudSelection = document.createElement("div") as HTMLDivElement;
     cloudSelection.draggable = false;
-    cloudSelection.style.position = 'absolute';
+    cloudSelection.style.position = "absolute";
     container.append(cloudSelection);
 
     let intersectionsNotes: Element[] = [];
-    let notes = Array.from(document.querySelectorAll(`.${itemsClass}`)) as HTMLElement[];
-    const notesInitialStyle = notes[0].getAttribute('style') ?? '';
-    const notesInitialClass = notes[0].getAttribute('class') ?? '';
+    let notes = Array.from(
+      document.querySelectorAll(`.${itemsClass}`)
+    ) as HTMLElement[];
+    const notesInitialStyle = notes[0].getAttribute("style") ?? "";
+    const notesInitialClass = notes[0].getAttribute("class") ?? "";
+    notesInitialAttributes.style = notes[0].getAttribute("style") ?? "";
+    notesInitialAttributes.class = notes[0].getAttribute("class") ?? "";
     notes.forEach((note) => {
       if (!intersectionsNotes.includes(note)) {
-        note.setAttribute('style', notesInitialStyle);
-        note.setAttribute('class', notesInitialClass);
+        note.setAttribute("style", notesInitialStyle);
+        note.setAttribute("class", notesInitialClass);
       }
     });
 
@@ -35,16 +57,17 @@ export default function SelectWithCloud({ itemsClass, containerClass, cloudStyle
         (note) =>
           cloud.offsetLeft <= note.offsetLeft + note.offsetWidth &&
           cloud.offsetHeight + cloud.offsetTop >= note.offsetTop &&
-          cloud.offsetTop <= note.offsetHeight + note.offsetTop,
+          cloud.offsetTop <= note.offsetHeight + note.offsetTop
       );
 
       intersections.forEach((note): void => {
         crossEffect({ style: note.style, classList: note.classList });
+        note.classList.add("cloud-selected-item");
       });
       notes.forEach((note) => {
         if (!intersectionsNotes.includes(note)) {
-          note.setAttribute('style', notesInitialStyle);
-          note.setAttribute('class', notesInitialClass);
+          note.setAttribute("style", notesInitialStyle);
+          note.setAttribute("class", notesInitialClass);
         }
       });
       intersectionsNotes = intersections;
@@ -52,88 +75,115 @@ export default function SelectWithCloud({ itemsClass, containerClass, cloudStyle
     resizeObserver.observe(cloudSelection);
 
     const startPosition = { x: 0, y: 0, pureY: 0, scrollY: 0 };
-    let scrollingActive = false;
+
     const mouseMoveHandler = (e: MouseEvent) => {
-      if (cloudSelection.offsetHeight > 100 && e.clientY > window.innerHeight * 0.9) {
+      if (
+        cloudSelection.offsetHeight > 100 &&
+        e.clientY > window.innerHeight * 0.9
+      ) {
         window.scrollTo({
           top: window.scrollY + document.documentElement.scrollHeight * 0.3,
-          behavior: 'smooth',
+          behavior: "smooth"
         });
         // console.log("scrolling bottom");
-        scrollingActive = true;
       }
-      if (cloudSelection.offsetHeight > 100 && e.clientY < window.innerHeight * 0.1 && window.scrollY !== 0) {
+      if (
+        cloudSelection.offsetHeight > 100 &&
+        e.clientY < window.innerHeight * 0.1 &&
+        window.scrollY !== 0
+      ) {
         window.scrollTo({
           top: window.scrollY - document.documentElement.scrollHeight * 0.3,
-          behavior: 'smooth',
+          behavior: "smooth"
         });
-
-        scrollingActive = true;
       }
 
       if (e.clientX < startPosition.x) {
-        cloudSelection.style.left = e.clientX - container.offsetLeft + 'px';
-        cloudSelection.style.width = startPosition.x - e.clientX + 'px';
+        cloudSelection.style.left = e.clientX - container.offsetLeft + "px";
+        cloudSelection.style.width = startPosition.x - e.clientX + "px";
       } else {
-        cloudSelection.style.left = startPosition.x - container.offsetLeft + 'px';
-        cloudSelection.style.width = e.clientX - startPosition.x + 'px';
+        cloudSelection.style.left =
+          startPosition.x - container.offsetLeft + "px";
+        cloudSelection.style.width = e.clientX - startPosition.x + "px";
       }
       if (e.clientY < startPosition.y - container.offsetTop - window.scrollY) {
         // console.log("up");
-        cloudSelection.style.top = e.clientY + window.scrollY - container.offsetTop + 'px';
-        cloudSelection.style.height = startPosition.y - container.offsetTop - window.scrollY - e.clientY + 'px';
+        cloudSelection.style.top =
+          e.clientY + window.scrollY - container.offsetTop + "px";
+        cloudSelection.style.height =
+          startPosition.y -
+          container.offsetTop -
+          window.scrollY -
+          e.clientY +
+          "px";
       } else {
         // console.log("down");
 
-        cloudSelection.style.top = startPosition.pureY - container.offsetTop + startPosition.scrollY + 'px';
+        cloudSelection.style.top =
+          startPosition.pureY -
+          container.offsetTop +
+          startPosition.scrollY +
+          "px";
         cloudSelection.style.height =
-          e.clientY - cloudSelection.offsetTop - container.offsetTop + window.scrollY + 'px';
+          e.clientY -
+          cloudSelection.offsetTop -
+          container.offsetTop +
+          window.scrollY +
+          "px";
       }
     };
     const mouseDownHandler = (e: MouseEvent) => {
       document.documentElement.draggable = false;
-      document.documentElement.style.userSelect = 'none';
-      notes = Array.from(document.querySelectorAll(`.${itemsClass}`)) as HTMLElement[];
+      document.documentElement.style.userSelect = "none";
+      notes = Array.from(
+        document.querySelectorAll(`.${itemsClass}`)
+      ) as HTMLElement[];
       notes.forEach((note) => {
         if (!intersectionsNotes.includes(note)) {
-          note.setAttribute('style', notesInitialStyle);
-          note.setAttribute('class', notesInitialClass);
+          note.setAttribute("style", notesInitialStyle);
+          note.setAttribute("class", notesInitialClass);
         }
       });
       cloudSelection.setAttribute(
-        'style',
-        `position:absolute; ${
-          cloudStyle
-            ? cloudStyle
-            : 'background:rgb(9, 113, 241,.2) ;  border-radius:5px;border: 2px solid rgb(9, 113, 241,.4)'
-        }`,
+        "style",
+        `position:absolute; ${cloudStyle
+          ? cloudStyle
+          : "background:rgb(9, 113, 241,.2) ;  border-radius:5px;border: 2px solid rgb(9, 113, 241,.4)"
+        }`
       );
 
-      cloudSelection.style.left = e.clientX + 'px';
-      cloudSelection.style.top = container.offsetTop + e.clientY + window.scrollY + 'px';
+      cloudSelection.style.left = e.clientX + "px";
+      cloudSelection.style.top =
+        container.offsetTop + e.clientY + window.scrollY + "px";
       startPosition.scrollY = window.scrollY;
       startPosition.x = e.clientX;
       startPosition.pureY = e.clientY;
       startPosition.y = container.offsetTop + e.clientY + window.scrollY;
-      container.addEventListener('mousemove', mouseMoveHandler);
+      container.addEventListener("mousemove", mouseMoveHandler);
     };
     const mouseUpHandler = () => {
-      scrollingActive = false;
-
       document.documentElement.draggable = true;
-      document.documentElement.style.userSelect = 'auto';
-      cloudSelection.setAttribute('style', 'position:absolute;');
+      document.documentElement.style.userSelect = "auto";
+      cloudSelection.setAttribute("style", "position:absolute;");
       onComplete(intersectionsNotes);
-      container.removeEventListener('mousemove', mouseMoveHandler);
+      container.removeEventListener("mousemove", mouseMoveHandler);
     };
     const mouseLeaveHandler = (e: MouseEvent) => {
       // cloudSelection.setAttribute("style", "");
       // container.removeEventListener("mousemove", mouseMoveHandler);
     };
 
-    container.addEventListener('mousedown', mouseDownHandler);
-    container.addEventListener('mouseup', mouseUpHandler);
-    container.addEventListener('mouseleave', mouseLeaveHandler);
+    container.addEventListener("mousedown", mouseDownHandler);
+    container.addEventListener("mouseup", mouseUpHandler);
+    container.addEventListener("mouseleave", mouseLeaveHandler);
     runnedSelectWithCloud = true;
   }
+}
+
+export function clean() {
+  const selectedItems = document.querySelectorAll(".cloud-selected-item");
+  selectedItems.forEach((item) => {
+    item.setAttribute("style", notesInitialAttributes.style);
+    item.setAttribute("class", notesInitialAttributes.class);
+  });
 }
