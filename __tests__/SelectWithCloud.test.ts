094dc226838306daa7b27aa43621d1d91d487bec
fatih255 <fatih969692@gmail.com>
2022-09-16 22:44:32 +0300

    test

--- __tests__/SelectWithCloud.test.ts
+++ __tests__/SelectWithCloud.test.ts
@@ -3,15 +3,17 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import ReactTest from '../src/ReactTest';
 
+jest.mock('../src/ReactTest');
 global.ResizeObserver = jest.fn().mockImplementation(() => ({
     observe: jest.fn(),
     unobserve: jest.fn(),
     disconnect: jest.fn(),
 }))
-it('Select Cloud Component Test Render with React', () => {
 
-    const Test = new ReactTest({}) as unknown as React.ReactElement<any, string | React.JSXElementConstructor<any>>;
-    const component = renderer.create(Test);
+it('We can check if the consumer called the class constructor', () => {
+    const Test = new ReactTest({}).render();
+    const component = renderer.create(Test)
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
-})
+
+});
