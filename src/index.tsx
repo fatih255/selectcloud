191da0ee09fd857b862ba781852ef0fb95547858
fatih255 <fatih969692@gmail.com>
2022-09-16 23:41:32 +0300

    1.0.21

--- src/index.tsx
+++ src/index.tsx
@@ -1,39 +1,36 @@
-import React, { Component } from "react";
-import SelectCloudWorker, { CloudProps } from "./SelectCloudWorker";
+import React, { Component } from 'react';
+import SelectCloudWorker, { CloudProps } from './SelectCloudWorker';
 
 export default class SelectCloud extends Component<Props, State> {
-	static defaultProps = {
-		selectable: true
-	};
+  static defaultProps = {
+    selectable: true,
+  };
 
-	constructor(props: Props) {
-		super(props);
-		this.state = {
-			// vv: vv
-		};
-	}
-	render() {
-		return (
-			<div>
-				{this.props.selectable ? (
-					<SelectCloudWorker
-						children={this.props.children}
-						options={this.props.options}
-					/>
-				) : (
-					this.props.children
-				)}
-			</div>
-		);
-	}
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      // vv: vv
+    };
+  }
+  render() {
+    return (
+      <div>
+        {this.props.selectable ? (
+          <SelectCloudWorker children={this.props.children} options={this.props.options} />
+        ) : (
+          this.props.children
+        )}
+      </div>
+    );
+  }
 }
 
 type Props = typeof SelectCloud.defaultProps & {
-	options: CloudProps["options"];
-	children?: React.ReactNode;
-	selectable?: boolean;
+  options: CloudProps['options'];
+  children?: React.ReactNode;
+  selectable?: boolean;
 };
 
 type State = {
-	// state
+  // state
 };
