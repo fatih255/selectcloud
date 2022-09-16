ad6e044f837604f0ed59673d3d2978707cde63ab
fatih255 <fatih969692@gmail.com>
2022-09-16 18:24:49 +0300

    change comcept

--- __tests__/SelectWithCloud.test.ts
+++ __tests__/SelectWithCloud.test.ts
@@ -1,4 +1,5 @@
 
+import React from 'react';
 import renderer from 'react-test-renderer';
 import ReactTest from '../src/ReactTest';
 
@@ -9,8 +10,8 @@ global.ResizeObserver = jest.fn().mockImplementation(() => ({
 }))
 it('Select Cloud Component Test Render with React', () => {
 
-
-    const component = renderer.create(ReactTest);
+    const Test = new ReactTest({}) as unknown as React.ReactElement<any, string | React.JSXElementConstructor<any>>;
+    const component = renderer.create(Test);
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
 })
