07201c790b612e9353de8aa4bbbb55054eeee6fa
fatih255 <fatih969692@gmail.com>
2022-09-17 23:28:46 +0300

    1.0.29

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,20 @@
-import React, { Component } from "react";
-import SelectCloudWorker, { CloudProps } from "./SelectCloudWorker";
+import React, { Component } from 'react';
+import SelectCloudWorker, { CloudProps } from './SelectCloudWorker';
 
 export default class SelectCloud extends Component<Props, State> {
   static defaultProps = {
-    selectable: true
+    selectable: true,
   };
   componentDidMount() {
     const observer = new MutationObserver((mutationList, o) => {
       const haveitem = mutationList
-        .filter((m) => m.type === "childList")
+        .filter((m) => m.type === 'childList')
         .reduce((prev: any, next: any) => [...prev, next.addedNodes], [])
         .map((rnode) => {
           const isItemClassElements: any[] = [];
           const nodes = rnode[0].querySelectorAll(`*`);
-          nodes.forEach((node: { getAttribute: (arg0: string) => string | string[]; }) => {
-            isItemClassElements.push(
-              node
-                .getAttribute("class")
-                ?.includes(this.props.options.itemsClass)
-            );
+          nodes.forEach((node: { getAttribute: (arg0: string) => string | string[] }) => {
+            isItemClassElements.push(node.getAttribute('class')?.includes(this.props.options.itemsClass));
           });
           return isItemClassElements.some((x) => x);
         })
@@ -29,15 +25,13 @@ export default class SelectCloud extends Component<Props, State> {
         return;
       }
     });
-    const targetNode = document.querySelector(
-      `.${this.props.options.containerClass}`
-    );
+    const targetNode = document.querySelector(`.${this.props.options.containerClass}`);
     if (targetNode) observer.observe(targetNode, { childList: true });
   }
   constructor(props: Props) {
     super(props);
     this.state = {
-      haveNode: false
+      haveNode: false,
     };
   }
 
@@ -45,23 +39,17 @@ export default class SelectCloud extends Component<Props, State> {
     return (
       <>
         {this.props.selectable && this.state.haveNode && (
-          <SelectCloudWorker
-            children={this.props.children}
-            options={this.props.options}
-          />
+          <SelectCloudWorker children={this.props.children} options={this.props.options} />
         )}
-        {(!this.props.selectable || !this.state.haveNode) &&
-          this.props.children}
-        {!this.state.haveNode &&
-          this.props.loadingComponent &&
-          this.props.loadingComponent}
+        {(!this.props.selectable || !this.state.haveNode) && this.props.children}
+        {!this.state.haveNode && this.props.loadingComponent && this.props.loadingComponent}
       </>
     );
   }
 }
 
 type Props = typeof SelectCloud.defaultProps & {
-  options: CloudProps["options"];
+  options: CloudProps['options'];
   children?: React.ReactNode;
   selectable?: boolean;
   loadingComponent?: JSX.Element;
