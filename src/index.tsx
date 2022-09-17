9d5b013b996dfd58563751cc429822c9a305d498
fatih255 <fatih969692@gmail.com>
2022-09-17 03:51:02 +0300

    1.0.27

--- src/index.tsx
+++ src/index.tsx
@@ -1,62 +1,52 @@
-import React, { Component } from "react";
-import SelectCloudWorker, { CloudProps } from "./SelectCloudWorker";
+import React, { Component } from 'react';
+import SelectCloudWorker, { CloudProps } from './SelectCloudWorker';
 
 export default class SelectCloud extends Component<Props, State> {
-	static defaultProps = {
-		selectable: true
-	};
-	componentDidMount() {
-		const observer = new MutationObserver((mutationList, o) => {
-			const haveitem = mutationList
-				.filter((m) => m.type === "childList")
-				.reduce((prev: any, next: any) => [...prev, next.addedNodes], [])
-				.some((m) =>
-					m[0].getAttribute("class").includes(this.props.options.itemsClass)
-				);
-			if (haveitem) {
-				o.disconnect();
-				this.setState({ haveNode: true });
-				return;
-			}
-		});
-		const targetNode = document.querySelector(
-			`.${this.props.options.containerClass}`
-		);
-		if (targetNode) observer.observe(targetNode, { childList: true });
-	}
-	constructor(props: Props) {
-		super(props);
-		this.state = {
-			haveNode: false
-		};
-	}
+  static defaultProps = {
+    selectable: true,
+  };
+  componentDidMount() {
+    const observer = new MutationObserver((mutationList, o) => {
+      const haveitem = mutationList
+        .filter((m) => m.type === 'childList')
+        .reduce((prev: any, next: any) => [...prev, next.addedNodes], [])
+        .some((m) => m[0].getAttribute('class').includes(this.props.options.itemsClass));
+      if (haveitem) {
+        o.disconnect();
+        this.setState({ haveNode: true });
+        return;
+      }
+    });
+    const targetNode = document.querySelector(`.${this.props.options.containerClass}`);
+    if (targetNode) observer.observe(targetNode, { childList: true });
+  }
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      haveNode: false,
+    };
+  }
 
-	render() {
-		return (
-			<>
-				{this.props.selectable && this.state.haveNode && (
-					<SelectCloudWorker
-						children={this.props.children}
-						options={this.props.options}
-					/>
-				)}
-				{(!this.props.selectable || !this.state.haveNode) &&
-					this.props.children}
-				{!this.state.haveNode &&
-					this.props.loadingComponent &&
-					this.props.loadingComponent}
-			</>
-		);
-	}
+  render() {
+    return (
+      <>
+        {this.props.selectable && this.state.haveNode && (
+          <SelectCloudWorker children={this.props.children} options={this.props.options} />
+        )}
+        {(!this.props.selectable || !this.state.haveNode) && this.props.children}
+        {!this.state.haveNode && this.props.loadingComponent && this.props.loadingComponent}
+      </>
+    );
+  }
 }
 
 type Props = typeof SelectCloud.defaultProps & {
-	options: CloudProps["options"];
-	children?: React.ReactNode;
-	selectable?: boolean;
-	loadingComponent?: JSX.Element;
+  options: CloudProps['options'];
+  children?: React.ReactNode;
+  selectable?: boolean;
+  loadingComponent?: JSX.Element;
 };
 
 type State = {
-	haveNode: boolean;
+  haveNode: boolean;
 };
