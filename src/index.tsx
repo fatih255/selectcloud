4ea4f2bd0567656d6ddabe2dc5b04d75bac31a8c
fatih255 <fatih969692@gmail.com>
2022-09-18 00:51:36 +0300

    new feature notSelectableClasses

--- src/index.tsx
+++ src/index.tsx
@@ -1,61 +1,82 @@
-import React, { Component } from 'react';
-import SelectCloudWorker, { CloudProps } from './SelectCloudWorker';
+import React, { Component } from "react";
+import SelectCloudWorker, { CloudProps } from "./SelectCloudWorker";
 
 export default class SelectCloud extends Component<Props, State> {
-  static defaultProps = {
-    selectable: true,
-  };
-  componentDidMount() {
-    const observer = new MutationObserver((mutationList, o) => {
-      const haveitem = mutationList
-        .filter((m) => m.type === 'childList')
-        .reduce((prev: any, next: any) => [...prev, next.addedNodes], [])
-        .map((rnode) => {
-          const isItemClassElements: any[] = [];
+	static defaultProps = {
+		selectable: true
+	};
+	componentDidMount() {
+		const observer = new MutationObserver((mutationList, o) => {
+			const haveitem = mutationList
+				.filter((m) => m.type === "childList")
+				.reduce((prev: any, next: any) => [...prev, next.addedNodes], [])
+				.map((rnode) => {
+					const isItemClassElements: any[] = [];
+					isItemClassElements.push(
+						rnode[0]
+							.getAttribute("class")
+							?.includes(this.props.options.itemsClass)
+					);
+					const nodes = rnode[0].querySelectorAll(`*`);
 
-          const nodes = rnode[0].querySelectorAll(`*`);
-          nodes.forEach((node: { getAttribute: (arg0: string) => string | string[] }) => {
-            isItemClassElements.push(node.getAttribute('class')?.includes(this.props.options.itemsClass));
-          });
-          return isItemClassElements.some((x) => x);
-        })
-        .some((x) => x);
-      if (haveitem) {
-        o.disconnect();
-        this.setState({ haveNode: true });
-        return;
-      }
-    });
-    const targetNode = document.querySelector(`.${this.props.options.containerClass}`);
-    if (targetNode) observer.observe(targetNode, { childList: true, subtree: true });
-  }
-  constructor(props: Props) {
-    super(props);
-    this.state = {
-      haveNode: false,
-    };
-  }
+					nodes.forEach(
+						(node: { getAttribute: (arg0: string) => string | string[] }) => {
+							isItemClassElements.push(
+								node
+									.getAttribute("class")
+									?.includes(this.props.options.itemsClass)
+							);
+						}
+					);
 
-  render() {
-    return (
-      <>
-        {this.props.selectable && this.state.haveNode && (
-          <SelectCloudWorker children={this.props.children} options={this.props.options} />
-        )}
-        {(!this.props.selectable || !this.state.haveNode) && this.props.children}
-        {!this.state.haveNode && this.props.loadingComponent && this.props.loadingComponent}
-      </>
-    );
-  }
+					return isItemClassElements.some((x) => x);
+				})
+				.some((x) => x);
+			if (haveitem) {
+				o.disconnect();
+				this.setState({ haveNode: true });
+				return;
+			}
+		});
+		const targetNode = document.querySelector(
+			`.${this.props.options.containerClass}`
+		);
+		if (targetNode)
+			observer.observe(targetNode, { childList: true, subtree: true });
+	}
+	constructor(props: Props) {
+		super(props);
+		this.state = {
+			haveNode: false
+		};
+	}
+
+	render() {
+		return (
+			<>
+				{this.props.selectable && this.state.haveNode && (
+					<SelectCloudWorker
+						children={this.props.children}
+						options={this.props.options}
+					/>
+				)}
+				{(!this.props.selectable || !this.state.haveNode) &&
+					this.props.children}
+				{!this.state.haveNode &&
+					this.props.loadingComponent &&
+					this.props.loadingComponent}
+			</>
+		);
+	}
 }
 
 type Props = typeof SelectCloud.defaultProps & {
-  options: CloudProps['options'];
-  children?: React.ReactNode;
-  selectable?: boolean;
-  loadingComponent?: JSX.Element;
+	options: CloudProps["options"];
+	children?: React.ReactNode;
+	selectable?: boolean;
+	loadingComponent?: JSX.Element;
 };
 
 type State = {
-  haveNode: boolean;
+	haveNode: boolean;
 };
