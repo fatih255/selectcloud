2f6282d103bd6d57f91bf8801a6e5f1ed11a0e2b
fatih255 <fatih969692@gmail.com>
2022-09-18 00:52:14 +0300

    1.0.32

--- src/ReactTest.tsx
+++ src/ReactTest.tsx
@@ -1,35 +1,33 @@
-import * as React from "react";
-import { render } from "react-dom";
-import SelectCloud from ".";
-import { clean } from "./SelectCloudWorker";
+import * as React from 'react';
+import { render } from 'react-dom';
+import SelectCloud from '.';
+import { clean } from './SelectCloudWorker';
 type Props = {};
 
 type State = {
   selectable: boolean;
   todos:
-  | {
-    content: string;
-    isCompleted: boolean;
-    id: string;
-  }[]
-  | null;
+    | {
+        content: string;
+        isCompleted: boolean;
+        id: string;
+      }[]
+    | null;
 };
-const APIBASE_URL = "https://631e2890cc652771a4928394.mockapi.io";
+const APIBASE_URL = 'https://631e2890cc652771a4928394.mockapi.io';
 
 export default class ReactTest extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props);
     this.state = {
       selectable: true,
-      todos: []
+      todos: [],
     };
   }
   async componentDidMount() {
-    const todos = await (
-      await fetch(`${APIBASE_URL}/todos`, { method: "GET" })
-    ).json();
+    const todos = await (await fetch(`${APIBASE_URL}/todos`, { method: 'GET' })).json();
     this.setState({
-      todos: [...todos]
+      todos: [...todos],
     });
   }
 
@@ -40,25 +38,25 @@ export default class ReactTest extends React.Component<Props, State> {
           selectable={this.state.selectable}
           loadingComponent={<div>Loading...</div>}
           options={{
-            itemsClass: "note",
-            containerClass: "cloud-selection-box",
-            notSelectableClasses: ["action-box", "tessasc"],
+            itemsClass: 'note',
+            containerClass: 'cloud-selection-box',
+            notSelectableClasses: ['action-box', 'tessasc'],
             crossEffect: ({ style, classList }) => {
-              style.color = "yellow";
-              classList.add("test");
+              style.color = 'yellow';
+              classList.add('test');
             },
             onComplete: ({ getAttributes, elements }) => {
               // console.log(getAttributes("data-id"));
-            }
-          }}>
+            },
+          }}
+        >
           <div
             style={{
               height: 200,
-              background: "yellow"
-            }}></div>
-          <div
-            style={{ marginLeft: 50, marginRight: 50 }}
-            className="cloud-selection-box">
+              background: 'yellow',
+            }}
+          ></div>
+          <div style={{ marginLeft: 50, marginRight: 50 }} className="cloud-selection-box">
             <div>
               <div>
                 {this.state.todos &&
@@ -70,13 +68,14 @@ export default class ReactTest extends React.Component<Props, State> {
                           key={item.id}
                           data-id={item.id}
                           style={{
-                            flexDirection: "row",
-                            display: "flex",
-                            background: "red",
-                            width: "100%",
-                            height: 100
+                            flexDirection: 'row',
+                            display: 'flex',
+                            background: 'red',
+                            width: '100%',
+                            height: 100,
                           }}
-                          className="note">
+                          className="note"
+                        >
                           <span style={{ flex: 1 }}>{item.content}</span>
                           <span className="tessasc">test</span>
                           <div
@@ -84,11 +83,14 @@ export default class ReactTest extends React.Component<Props, State> {
                             style={{
                               flex: 1,
                               width: 100,
-                              background: "yellow"
-                            }}>
-                            <button onClick={() => {
-                              // console.log("clicked")
-                            }}>
+                              background: 'yellow',
+                            }}
+                          >
+                            <button
+                              onClick={() => {
+                                // console.log("clicked")
+                              }}
+                            >
                               test
                             </button>
                           </div>
@@ -100,23 +102,20 @@ export default class ReactTest extends React.Component<Props, State> {
             </div>
           </div>
         </SelectCloud>
-        <button onClick={() => this.setState({ selectable: true })}>
-          selectable
-        </button>
-        <button onClick={() => this.setState({ selectable: false })}>
-          selectablenone
-        </button>
+        <button onClick={() => this.setState({ selectable: true })}>selectable</button>
+        <button onClick={() => this.setState({ selectable: false })}>selectablenone</button>
         <button onClick={() => clean()}>remove selection</button>
         <button
           onClick={() => {
-            const selectionbox = document.querySelector(".cloud-selection-box");
-            const newitem = document.createElement("h1") as HTMLElement;
-            newitem.innerText = "new";
-            newitem.classList.add("note");
-            newitem.style.width = 100 + "px";
-            newitem.style.backgroundColor = "red";
+            const selectionbox = document.querySelector('.cloud-selection-box');
+            const newitem = document.createElement('h1') as HTMLElement;
+            newitem.innerText = 'new';
+            newitem.classList.add('note');
+            newitem.style.width = 100 + 'px';
+            newitem.style.backgroundColor = 'red';
             if (selectionbox) selectionbox.append(newitem);
-          }}>
+          }}
+        >
           add Item
         </button>
       </div>
@@ -124,4 +123,4 @@ export default class ReactTest extends React.Component<Props, State> {
   }
 }
 
-render(<ReactTest />, document.getElementById("root"));
+render(<ReactTest />, document.getElementById('root'));
